2d2e5ea1978edcb7af47ee763efdf94194dd3df7
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-08-20 16:55:52 +0900

    login modal added

---
+++ src/components/LoginModals/LoginModal.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./LoginModal.css";
+
+const LoginModal = (props) => {
+  const { open, close } = props;
+  console.log(props);
+
+  return (
+    <div className={open ? "openModal modal" : "modal"}>
+      {open ? (
+        <section>
+          <header>
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+          </header>
+          <main>{props.children}</main>
+        </section>
+      ) : null}
+    </div>
+  );
+};
+
+export default LoginModal;
