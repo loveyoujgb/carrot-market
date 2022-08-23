0d6a9fa26d638dc3542b59ff572923d3245ccff9
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-08-23 15:26:15 +0900

    token state added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import logo from "../img/logo.png";
 import { useNavigate } from "react-router-dom";
@@ -7,9 +7,16 @@ import KakaoImg from "../img/KakaoImg.png";
 import { IsLogin, Logout } from "../shared/isLogin";
 
 const Header = (props) => {
-  const [IsLogin, setIsLogin] = useState(false);
+  // const [IsLogin, setIsLogin] = useState(false);
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT}&response_type=code`;
   const navigate = useNavigate();
+  const [token, setToken] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setToken(token);
+  }, [token]);
+
   const [modalOpen, setModalOpen] = useState(false);
   const openModal = () => {
     setModalOpen(true);
@@ -23,6 +30,7 @@ const Header = (props) => {
 
   const onClickDelete = () => {
     Logout();
+    setToken(null);
   };
   console.log(IsLogin);
   return (
@@ -44,7 +52,7 @@ const Header = (props) => {
           </Title>
         </LogoLink>
         <RightWrap>
-          {IsLogin !== null ? (
+          {token ? (
             <button
               style={{ border: "1px solid #bbbbbb", fontWeight: "bold" }}
               className="button is-white"
@@ -64,8 +72,14 @@ const Header = (props) => {
           <LoginModal open={modalOpen} close={closeModal}>
             <main>
               <ButtonInModalWrap>
-                <img style={{ width: "50%", marginTop: "8%" }} src={logo} alt="이미지" />
-                <h2 style={{ fontWeight: "bold" }}>우리 동네 중고 직거래 마켓</h2>
+                <img
+                  style={{ width: "50%", marginTop: "8%" }}
+                  src={logo}
+                  alt="이미지"
+                />
+                <h2 style={{ fontWeight: "bold" }}>
+                  우리 동네 중고 직거래 마켓
+                </h2>
                 <p style={{ fontSize: "0.8rem", textAlign: "center" }}>
                   내 동네를 설정하고 <br /> 당근마켓을 시작해보세요.
                 </p>
@@ -79,7 +93,10 @@ const Header = (props) => {
               </ButtonInModalWrap>
             </main>
           </LoginModal>
-          <button style={{ border: "1px solid #bbbbbb", fontWeight: "bold" }} className="button is-white">
+          <button
+            style={{ border: "1px solid #bbbbbb", fontWeight: "bold" }}
+            className="button is-white"
+          >
             회원가입
           </button>
         </RightWrap>
