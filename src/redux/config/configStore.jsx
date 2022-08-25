7b22cc46a8c1fd4cc17d1f203d837e96fcb4677e
Danbee Kim <danbeekim@Danbeeui-MacBookAir.local>
2022-08-25 14:41:55 +0900

    deleted module and added devtool

--- src/redux/config/configStore.jsx
+++ src/redux/config/configStore.jsx
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import modules from "../modules/modulesSlice";
 import detail from "../modules/detailSlice";
 import comments from "../modules/commentsSlice"
 import lists from "../modules/listSlice"
 
 export const store = configureStore({
-  // devTools: process.env.NODE_ENV !== "production",
+  devTools: process.env.NODE_ENV !== "production",
   reducer: {
-    modules,
     detail,
     comments,
     lists,
